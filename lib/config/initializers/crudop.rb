b51960e410bd7bfdbfea6b4696b0ca743bf15443
tanzim10 <tanzimfarhan@gmail.com>
2024-01-08 14:27:36 +0600

    Dynamo CRUD done

--- lib/config/initializers/crudop.rb
+++
@@ -1,6 +0,0 @@
-Crudop.configure do |config|
-    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    config.aws_region = ENV['AWS_REGION']
-  end
-
