a78071e146eb32b86786cc3456ec6d9f8d0ea079
tanzim10 <tanzimfarhan@gmail.com>
2024-03-22 00:58:40 +0600

    Test Cases issues resolved

--- spec/lib/dynamodb_spec.rb
+++ spec/lib/dynamodb_spec.rb
@@ -2,13 +2,13 @@ require "spec_helper"
 require "crudop"
 
 RSpec.describe Crudop::Dynamo do
+  let (:test_table_name) {'dynamo'}
+  let (:Pkey) {'test_key'}
+  let (:Skey) {'sort_key'}
+  let (:pval) {'test'}
+  let (:sval) {'sort'}
+  let (:test_item) {{id: '123', timestamp: 456, data: 'test_data'}}
 
-  include_context "table setup"
-
-  
-  let(:test_record) do
-    Crudop::Dynamo.dy_get_item(test_table_name, { "test_key" => @key })[:item]
-  end
   
   describe ".dynamodb_client" do
     it "returns a DynamoDB client" do
@@ -18,65 +18,80 @@ RSpec.describe Crudop::Dynamo do
   
 
   describe ".dy_put_item" do
-
     it "creates an item in the table" do
-      Crudop::Dynamo.dy_put_item('dynamo', { 'test_key' => 'test' })
-      expect(test_record). to be_nill
+      dynamo_client_double = instance_double(Aws::DynamoDB::Client)
+      allow(Crudop::Dynamo).to receive(:dynamodb_client).and_return(dynamo_client_double)
+      
+      allow(dynamo_client_double).to receive(:put_item).and_return('mock_response')
+  
+      Crudop::Dynamo.dy_put_item(test_table_name, test_item)
+  
+      expect(dynamo_client_double).to have_received(:put_item).with(hash_including(table_name: test_table_name, item: test_item))
     end
   end
+  
 
   describe ".dy_update_item" do
+
+    let(:table_name) { 'dynamo' }
+    let(:key) { 'test_key' }
     let(:key_hash) { { 'test_key' => key } }
-    let(:new_attributes) {{ 'some_old_field' => 'new field', 'name'=> 'updated name', 'attribute' => 'updated attribute' }}
+    let(:new_attributes) {{ 'some_old_field' => 'new field', 'name' => 'updated name', 'attribute' => 'updated attribute' }}
+    let(:dynamo_client_double) { instance_double(Aws::DynamoDB::Client) }
   
-    before(:each) do
-      #updating the test record
-      Crudop::Dynamo.dy_put_item(@test_table_name, @test_item)
+    before do
+      # Stub the dynamodb_client to return the double
+      allow(Crudop::Dynamo).to receive(:dynamodb_client).and_return(dynamo_client_double)
+  
+      # Stub the update_item method on the DynamoDB client double
+      allow(dynamo_client_double).to receive(:update_item).and_return('mock_response')
     end
   
-    context "does not replace exising record" do
-  
-      before(:each) do
-        Crudop::Dynamo.dy_update_item(@test_table_name, key_hash, new_attributes)
+    it "updates existing columns and adds new columns without replacing existing records" do
+      # Perform the update operation
+      Crudop::Dynamo.dy_update_item(table_name, key_hash, new_attributes)
+    
+      # Verify update_item was called with expected arguments
+      expect(dynamo_client_double).to have_received(:update_item) do |args|
+        expect(args[:table_name]).to eq(table_name)
+        expect(args[:key]).to eq(key_hash)
+    
+        # Checking that each attribute is correctly referenced in the update expression
+        expect(args[:update_expression]).to match(/#SOME_OLD_FIELD\s*=\s*:valsome_old_field/)
+        expect(args[:update_expression]).to match(/#NAME\s*=\s*:valname/)
+        expect(args[:update_expression]).to match(/#ATTRIBUTE\s*=\s*:valattribute/)
+    
+        # Checking that expression attribute names and values contain expected mappings
+        expect(args[:expression_attribute_names]).to include("#SOME_OLD_FIELD" => "some_old_field")
+        expect(args[:expression_attribute_values]).to include(":valsome_old_field" => anything)
       end
-  
-      it "will update exisiting columns" do
-        expect(test_record['name']).to eq(new_attributes['name'])
-      end
-  
-      it "will add the new columns " do
-        expect(test_record['some_old_field']).to eq(new_attributes['some_old_field'])
-        expect(test_record['attribute']).to eq(new_attributes['attribute'])
-      end
-  
-      it "does not change columns that were not updated" do
-        expect(test_record['attribute']).to eql @test_item['attribute']
-      end
-  
-    end
+    end    
   end
-
   
 
 
-  describe ".sanitize_dynamo_hash" do
 
+  describe ".sanitize_hash" do
+    let(:time_now) { Time.now }
     let(:test_hash) do
       {
-        'hash' => {'test1'=>'test1'},
-        'time' => Time.now,
-        'regular'=> 'regular',
+        'hash' => {'empty_string' => ''},
+        'time' => time_now,
+        'regular' => 'regular',
+        'empty' => ''
       }
     end
-    
-    it "returns the DynamoDB valid hash" do
-      hash = Crudop::Dynamo.sanitize_dynamo_hash(test_hash)
-      expect(hash['hash']). to eql test_hash['hash']
-      expect(hash['time']). to eql test_hash['time'].iso8601
-      expect(hash['regular']). to eql test_hash['regular']
-    end
   
+    it "returns a DynamoDB valid hash with sanitized values" do
+      sanitized_hash = Crudop::Dynamo.sanitize_hash(test_hash)
+  
+      expect(sanitized_hash['hash']).to eq({'empty_string' => "<empty string>"})
+      expect(sanitized_hash['time']).to eq(time_now.iso8601)
+      expect(sanitized_hash['regular']).to eq('regular')
+      expect(sanitized_hash['empty']).to eq("<empty string>")
+    end
   end
+  
 
 
 
